f698f475b5df3050b19ebe791778502c92be8c12
bryantparsons <58860704+bryantparsons@users.noreply.github.com>
2020-12-22 20:17:59 -0500

    adding (e) for T-shirt design

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 //Global variables for form
 const jobTitle = document.getElementById('title');
 const otherJobRole = document.getElementById('other-job-role');
+const shirtColor = document.getElementById('color');
+const shirtDesign = document.getElementById('design');
 
 otherJobRole.style.display = 'none';
+shirtColor.disabled = true;
 
 //Code to bring name input for form into focus
 
@@ -10,11 +13,13 @@ document.getElementById('name').focus({preventScroll:true});
 
 jobTitle.addEventListener('change', (e) => {
         const otherJobSelect = e.target;
-        otherJobSelect.value = 'other';
-        if (otherJobSelect) {
+        if (otherJobSelect.value === 'other') {
             otherJobRole.style.display = 'block';           
             } else {
             otherJobRole.style.display = 'none';
             }
+});
+
+shirtDesign.addEventListener('change', (e) => {
     
 });
